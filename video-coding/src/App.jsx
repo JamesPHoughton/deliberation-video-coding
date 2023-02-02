ef6c7698c6db2b20b94cf2ecb03abf8d249c27aa
James Houghton <james.p.houghton@gmail.com>
2023-02-02 10:22:54 -0500

    plays single video from url

---
+++ video-coding/src/App.jsx
@@ -0,0 +1,34 @@
+import { useState } from "react";
+import reactLogo from "./assets/react.svg";
+import "./App.css";
+
+function App() {
+  const [count, setCount] = useState(0);
+
+  return (
+    <div className="App">
+      <div>
+        <video id="myVideo" preload="auto" width="500" controls>
+          <source
+            src="https://dl8.webmfiles.org/big-buck-bunny_trailer.webm"
+            type="video/webm"
+          />
+        </video>
+      </div>
+      <h1>Vite + React</h1>
+      <div className="card">
+        <button onClick={() => setCount((count) => count + 1)}>
+          count is {count}
+        </button>
+        <p>
+          Edit <code>src/App.jsx</code> and save to test HMR
+        </p>
+      </div>
+      <p className="read-the-docs">
+        Click on the Vite and React logos to learn more
+      </p>
+    </div>
+  );
+}
+
+export default App;
